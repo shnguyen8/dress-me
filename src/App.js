58aceacba3204e79021d22b364f6fb0674b57ef3
Shirley Nguyen <shirley.nguyen8@outlook.com>
2022-08-09 11:19:02 -0500

    Initial commits

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        Dress Me
+      </header>
+
+      <p>
+        
+      </p>
+    </div>
+  );
+}
+
+export default App;

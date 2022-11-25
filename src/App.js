fd80922709fc81b0b0eb92fce98bcd03cb6d1323
Shirley Nguyen <shirley.nguyen8@outlook.com>
2022-11-24 21:52:40 -0600

    Reinitialized Application

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <header>
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
         Pup Track
       </header>
-
-      <p>
-
-      </p>
     </div>
   );
 }
